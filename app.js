eb90c62377bb5c90b080f038d040a39e46190044
Your Name <you@example.com>
2020-09-03 11:29:48 +0200

    app.js

--- app.js
+++ app.js
@@ -2,31 +2,11 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 
-  // Starting Monday not friday
- /* app.use(date=(req,res,next)=>{
-function dates(current) {
-    var week= new Array(); 
-     current.setDate((current.getDate() - current.getDay() +1));
-    for (var i = 0; i < 5; i++) {
-        week.push(
-            new Date(current)
-        ); 
-        current.setDate(current.getDate() +1);
-    }
-    return week; 
-    console.log(week)
-    next()
-}})*/
-//starting 08.00 to 17.00
-app.use(date=(req,res,next)=>{
-    var date1 = new Date(  9); // 8:00 AM
-    var date2 = new Date( 17); // 17:00 PM
-    if (date2< date1) {
-        date1.setDate(date1.getDate() + 1);
-    }
-    var diff = date2 - date1;
-    console.log(diff)
-next()
+app.use(verifytime=(req,res,next)=>{
+    const date =new Date();
+    console.log(date);
+    if (date.getDay() !== 0 && date.getDay() !== 6 && date.getHours()>=9 && date.getHours()<=17) {next()}
+    else {res.send("page not available : you are outside of working time..........")}
 })
 app.use(date=(req,res,next)=>{
  let requestAt=new Date()
